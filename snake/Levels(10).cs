8de8ecae8e85eb9f137515a5b4a1fa42a81de46e
ADMIN <daniel.mihol@gmail.com>
2020-06-25 07:06:08 +0300

    добавлена функция уровней

--- snake/Levels(10).cs
+++ snake/Levels(10).cs
@@ -8,7 +8,60 @@ namespace snake
 {
     class Levels_10
     {
-        public int x = 1;
-            
+        public int level;
+
+        public Levels_10(int x)
+        {
+            level = x;
+            Console.SetCursorPosition(12, 25);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("Level: " + level);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public Levels_10 AddLevel(Levels_10 level)
+        {
+            this.level++;
+            Console.SetCursorPosition(19, 25);
+            if (this.level == 2)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+            }
+            else if (this.level == 3)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+            }
+            else if (this.level == 4)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+            }
+            else if (this.level == 5)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+            }
+            else if (this.level == 6)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+            }
+            else if (this.level == 7)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+            }
+            else if (this.level == 8)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+            }
+            else if (this.level == 9)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+            }
+            else if (this.level == 10)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+            }
+            Console.Write(this.level);
+            Console.ForegroundColor = ConsoleColor.White;
+            return level;
+        }
     }
 }
